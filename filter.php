245552b26831c6557d63b8f36f5b0e6a4a8728a0
Antonio9026 <121766004+Antonio9026@users.noreply.github.com>
2023-09-06 19:51:59 +0200

    un po di css (pessimo)

--- filter.php
+++ filter.php
@@ -1,15 +1,33 @@
 <?php
 // echo isset($_POST["word"]) ? $_POST["word"] : "no words";
-$paragrafo= $_POST["paragrafo"];
+$paragrafo = $_POST["paragrafo"];
 $paragrafoLenght = strlen($paragrafo);
 
 $censoredWord = $_POST["censura"];
 $censoredLenght = strlen($censoredWord);
-$newCens = str_replace($censoredWord,"***", $paragrafo);
+$newCens = str_replace($censoredWord, "***", $paragrafo);
 
 
 ?>
+<!DOCTYPE html>
+<html lang="en">
 
-<h1><?php echo $paragrafo . " " . $paragrafoLenght  ?></h1>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/style.css">
+    <title>Document</title>
+</head>
 
-<h1><?php echo $newCens . " " . $censoredLenght ?></h1>
+<body>
+    <div class="container">
+        <div class="parag-filter">
+            <h1><?php echo $paragrafo . " " . $paragrafoLenght  ?></h1>
+        </div>
+        <div class="censored-filter">
+            <h1><?php echo $newCens . " " . $censoredLenght ?></h1>
+        </div>
+    </div>
+</body>
+
+</html>
--- index.php
+++ index.php
@@ -4,19 +4,26 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/style.css">
     <title>BadWords</title>
 </head>
 
 <body>
-    <form action="filter.php" method="POST">
-     
-            <label for="paragrafo">Paragrafo</label>
-            <input type="text" name="paragrafo">
-            <label for="censura">parola da censurare</label>
-            <input type="text" name="censura">
-       
-        <button type="submit">Invia</button>
-    </form>
+    <div class="form-container">
+        <form action="filter.php" method="POST">
+            <div class="paragrafo_container">
+                <label for="paragrafo"><strong>Paragrafo</strong></label>
+                <input type="text" name="paragrafo">
+            </div>
+            <div class="censura-container">
+                <label for="censura"><strong>Parola da censurare</strong></label>
+                <input type="text" name="censura">
+            </div>
+            <div class="button-container">
+                <button type="submit">Invia</button>
+            </div>
+        </form>
+    </div>
 </body>
 
 </html>
